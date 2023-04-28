cf317f6988b025e31d4ba483efe462a7f030a8ac
RaShunda Lanier <61757693+rlanier-webdev@users.noreply.github.com>
2023-04-28 13:45:26 -0500

    Update birthdays.py
    code cleanup, add error handling, add file header

--- birthdays.py
+++ birthdays.py
@@ -1,35 +1,60 @@
-import json
+'''
+Author: RaShunda Williams
+Project: Birthday Checker
+Description: A simple project that checks the user input against what's in the birthday db.
+    -- If the name is there, then print the birthday.
+    -- If the name is not there, then ask the user for the birthday to be added.
+        - save the new record to the data.json file
+IDEAS:
+    1. add error handling - DONE 4/27
+        - problem: if file doesn't exist, then it throws an error
+        - solution: create new file
+            -- new problem: error thrown because file is empty
+                'raise JSONDecodeError("Expecting value", s, err.value) from None
+                json.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0)'
+    2. add birthday validation - check to see if the input is a valid date
+    3. print the zodiac sign for the name user inputs
+    4. print the horoscope of the day
+'''
 
-file = "data.json"
+import json, time
 
-jsonFile = open(file,"r")
-jsonContent = jsonFile.read()
-birthdays = json.loads(jsonContent)
-    
-#save birthdays to json file
-def saveBirthday(birthdays):
-    jsonString = json.dumps(birthdays)
-    jsonFile = open(file, "w")
-    jsonFile.write(jsonString)
-    jsonFile.close
+filename = "data.json"
 
 while True:
-    print('Enter a name: (blank to quit)')
-    name = input()
+    try:
+        with open(filename,"r") as file:
+            content = file.read()
+            birthdays = json.loads(content)
+            
+        #save birthdays to json file
+        def saveBirthday(birthdays):
+            with open(filename, "w") as file:
+                json.dump(birthdays, file)
+
+        while True:
+            print('Enter a name: (blank to quit)')
+            name = input()
+            
+            if name == '':
+                break 
+
+            if name in birthdays:
+                print(birthdays[name] + ' is the birthday of ' + name)
+            else:
+                print('I do not have birthday information for ' + name)
+                
+                bday = input('When is their birthday? ')
+                birthdays[name] = bday
+
+                saveBirthday(birthdays)
+
+                print('Birthday database updated.')
+
+    except FileNotFoundError:
+        # If the file doesn't exist, create a new one
+        print('File does not exist. Creating file...')
+        open(filename, 'w')
+        print(f'{filename} - File created.')
     
-    if name == '':
-        break
-
-    if name in birthdays:
-        print(birthdays[name] + ' is the birthday of ' + name)
-    else:
-        print('I do not have birthday information for ' + name)
-        print('When is their birthday?')
-        
-        bday = input()
-        birthdays[name] = bday
-
-        saveBirthday(birthdays)
-
-        print('Birthday database updated.')
-
+    time.sleep(2)  # wait for 1 second before checking again
