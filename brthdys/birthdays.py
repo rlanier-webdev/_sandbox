0d689f18cf96a554c8742a28792de0626f9d6948
devray <developer@rashundawilliams.com>
2023-04-29 02:44:38 -0500

    refactor program for improved reliability, readability, and performance - Read JSON data directly from file, avoids memory issues and improves performance - The old way did not handle missing file errors and could cause the program to crash. - Use dictionary for simplified record creation and better readability - Append new records to existing list to avoid overwriting data - Remove unnecessary try-except blocks

--- brthdys/birthdays.py
+++ brthdys/birthdays.py
@@ -1,60 +1,72 @@
-'''
-Author: RaShunda Williams
-Project: Birthday Checker
-Description: A simple project that checks the user input against what's in the birthday db.
-    -- If the name is there, then print the birthday.
-    -- If the name is not there, then ask the user for the birthday to be added.
-        - save the new record to the data.json file
-IDEAS:
-    1. add error handling - DONE 4/27
-        - problem: if file doesn't exist, then it throws an error
-        - solution: create new file
-            -- new problem: error thrown because file is empty
-                'raise JSONDecodeError("Expecting value", s, err.value) from None
-                json.decoder.JSONDecodeError: Expecting value: line 1 column 1 (char 0)'
-    2. add birthday validation - check to see if the input is a valid date
-    3. print the zodiac sign for the name user inputs
-    4. print the horoscope of the day
-'''
+"""
+AUTHOR: RaShunda Williams
+PROJECT: Birthday Checker
+DESCRIPTION: A simple project that checks the user input against what's in the file.
+            -- If the name is there, then print the birthday.
+            -- If the name is not there, then ask the user for the birthday to be added.
+                - save the new record to the data.json file
 
-import json, time
+        TO DO:
+            - add error handling
+                - problem: if file doesn't exist, then it throws an error
+            - change bday to date format
+            - validate user input
+                - name should only include letters
+                - bday should be in date format, valid date
+            - print the zodiac sign for the name user inputs
+            - print the horoscope of the day
+        QUESTIONS:
+            - how should duplicates be handled?
+            -- if name matches mutliple records...
+"""
 
+import json
+
+#from json.decoder import JSONDecodeError
+
+# Initialize filename variable
 filename = "data.json"
 
-while True:
+while True: # keep prompting the user until they quit
     try:
+        # Load the initial records from file
         with open(filename,"r") as file:
-            content = file.read()
-            birthdays = json.loads(content)
-            
-        #save birthdays to json file
-        def saveBirthday(birthdays):
-            with open(filename, "w") as file:
-                json.dump(birthdays, file)
-
-        while True:
-            print('Enter a name: (blank to quit)')
-            name = input()
-            
-            if name == '':
-                break 
-
-            if name in birthdays:
-                print(birthdays[name] + ' is the birthday of ' + name)
-            else:
-                print('I do not have birthday information for ' + name)
-                
-                bday = input('When is their birthday? ')
-                birthdays[name] = bday
-
-                saveBirthday(birthdays)
-
-                print('Birthday database updated.')
-
+            records = json.load(file)
     except FileNotFoundError:
-        # If the file doesn't exist, create a new one
-        print('File does not exist. Creating file...')
-        open(filename, 'w')
-        print(f'{filename} - File created.')
+        # Create empty list of records if file doesn't exist
+        records = []
     
-    time.sleep(2)  # wait for 1 second before checking again
+    # Get user input for name
+    name = input("Enter a name: (blank to quit)")
+
+    # name is blank, quit
+    if name == "":
+        break
+
+    # Check if the name exists in the record
+    for record in records:
+        if record["name"] == name:
+            print(f"{name}'s Birthday is: ", record["bday"])
+            break
+    else:
+        # If the name does not exist in the record, get user input for birthday
+        bday = input("Enter their birthday: ")
+
+        # generate the record id
+        new_id = len(records) + 1
+
+        # create the new record
+        new_record = {"id": new_id, "name": name, "bday": bday}
+
+        # add the record
+        records.append(new_record)
+        
+        # write to file
+        with open(filename, "w") as file:
+            json.dump(records,file,indent = 4)
+
+        # Print the updated record list
+        print("New record added.")
+
+# except JSONDecodeError:  # includes json.decoder.JSONDecodeError
+#     print("Decoding JSON has failed")
